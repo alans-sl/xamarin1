4edd9521e2e93bfa58f71181e9ec46b8b4e7818a
Alan Stanley <astanley@scottlogic.co.uk>
2018-12-14 10:44:36 +0000

    grouped account list

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,50 @@ using Xamarin.Forms;
 
 namespace App1
 {
+    public class Account
+    {
+
+        public Account(String name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+
+    public class AccountTypeGroup : List<Account>
+    {
+        public string Title { get; set; }
+        private AccountTypeGroup(string title)
+        {
+            Title = title;
+        }
+
+        public static IList<AccountTypeGroup> All { private set; get; }
+
+        static AccountTypeGroup()
+        {
+            List<AccountTypeGroup> Groups = new List<AccountTypeGroup> {
+                new AccountTypeGroup ("My Savings"){
+                    new Account("Savings 1"),
+                    new Account("Savings 2")
+                },
+                new AccountTypeGroup ("My Mortgages"){
+                    new Account("Mortgage 1"),
+                    new Account("Mortgage 2")
+                }
+            };
+            All = Groups; //set the publicly accessible list
+        }
+    }
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            AccountsView.ItemsSource = AccountTypeGroup.All;
+
         }
     }
 }
